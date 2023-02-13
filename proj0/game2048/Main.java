56411c51d8ebacd495b016472039ca6d71111970
Omar Essam <omaressam1122333@gmail.com>
2023-02-13 23:45:33 +0200

    Added comment in main.java file

--- proj0/game2048/Main.java
+++ proj0/game2048/Main.java
@@ -8,7 +8,7 @@ import ucb.util.CommandArgs;
  *  @author P. N. Hilfinger
  */
 public class Main {
-
+//asda
     /** Number of squares on the side of a board. */
     static final int BOARD_SIZE = 4;
     /** Probability of choosing 2 as random tile (as opposed to 4). */
